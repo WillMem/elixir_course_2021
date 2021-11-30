9f3df9c167c64d2d72f1400a0af9e6ab418df0ec
amirOrbe <amir.orbe@bunsan.io>
2021-11-30 08:41:05 -0600

    fix

--- elixir_101_bunsan/lib/wordCount.ex
+++ elixir_101_bunsan/lib/wordCount.ex
@@ -1,17 +1,22 @@
-defmodule LinesCounter do
-
-    def count(path) do
-      File.read(path)
-      |> lines_num
-    end
-  
-    defp lines_num({:ok, contents}) do
-      contents
-      |> String.split("\n", trim: true)
-      |> length
-    end
-  
-    defp lines_num({:error, reason}) do
-      IO.puts("Error, reason: #{reason}")
-    end
+defmodule WordCount do
+  @moduledoc """
+  Provides a function `frequencies/1` to get the frequency of the words in the text
+  """
+  def count(path) do
+    File.read(path)
+    |> frequencies
   end
+
+  defp frequencies({:ok, text}) do
+    text
+    |> :unicode.characters_to_nfd_binary()
+    |> String.downcase()
+    |> String.replace(~r/[^a-z-\s]/u, "")
+    |> String.split([" ", "\n"], trim: true)
+    |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, fn value -> value + 1 end) end)
+  end
+
+  defp frequencies({:error, reason}) do
+    IO.puts("Error, reason: #{reason}")
+  end
+end
